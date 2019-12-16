9980dbfd25bbe0939f132979a027dd8a4e8ef1c2
Luiz Augusto Volpi Nascimento <lvolpi@celepar.pr.gov.br>
2019-12-16 14:51:28 -0300

    created getfilecontent function

--- gonfig.go
+++ gonfig.go
@@ -12,23 +12,15 @@ type unmarshalerFunc func(data []byte, v interface{}) error
 
 // Load a struct with the configuration from a config file.
 func Load(path string, config interface{}) error {
-	configFile, err := os.Open(path)
+	content, err := getFileContent(path)
 	if err != nil {
-		config = nil
-		return LoadError
-	}
-	defer configFile.Close()
-
-	b, err := ioutil.ReadAll(configFile)
-	if err != nil {
-		config = nil
 		return LoadError
 	}
 	unmarshaler, err := getUnmarchaler(path)
 	if err != nil {
 		return err
 	}
-	err = unmarshaler(b, &config)
+	err = unmarshaler(content, &config)
 	if err != nil {
 		config = nil
 		return LoadError
@@ -47,4 +39,14 @@ func getUnmarchaler(path string) (unmarshalerFunc, error) {
 	default:
 		return nil, UnsupportedFileError
 	}
+}
+
+func getFileContent(path string) (bytes []byte, err error) {
+	configFile, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	defer configFile.Close()
+	bytes, err = ioutil.ReadAll(configFile)
+	return
 }
